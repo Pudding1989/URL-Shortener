7cb7bdab72b0d34f55cbb7acb4a9ebfffa795fde
Pudding1989 <budding1989@outlook.com>
2021-09-12 00:21:43 +0800

    feat: 建立 model

---
+++ models/link.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const linkSchema = new Schema({
+  original: {
+    type: String,
+    required: true
+  },
+  shorten: {
+    type: String,
+    require: true
+  }
+  // NOTE: UTM應用可以作為之後 side project開發方向
+  // URL: ...網址?utm_source=facebook
+  // campaignSource: {
+  //   type: String,
+  // },
+  // campaignMedium: {
+  //   type: String,
+  // },
+  // campaignName: {
+  //   type: String,
+  // }
+})
+
+module.exports = mongoose.model('Link', linkSchema)
