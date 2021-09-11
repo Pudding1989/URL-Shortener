a6035d3af2e8f92dd0f5c4dd4fb127c7aa355bcd
Pudding1989 <budding1989@outlook.com>
2021-09-12 01:10:38 +0800

    feat: 設定程式入口

---
+++ app.js
@@ -0,0 +1,21 @@
+const PORT = process.env.PORT || 3000
+
+const express = require('express')
+const app = express()
+
+const exphbs = require('express-handlebars')
+
+const routes = require('./routes')
+
+require('./config/mongoose')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`Express Server listening on http://localhost:${PORT}`)
+  console.log('You can press『 ⌃ + C 』to disconnect Express Server')
+})
