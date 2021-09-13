71401b35200b7307faee4bff9e73510be8c6fa86
Pudding1989 <budding1989@outlook.com>
2021-09-13 21:46:20 +0800

    feat: 設定 main layout

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const Link = require('../../models/link')
 
 router.get('/', (req, res) => {
-  res.send('Router is operating')
+  res.render('index')
 })
 
 module.exports = router
